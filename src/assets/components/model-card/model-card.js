5e017c6960feeb692dea3be46ca7ff5b52650da1
Polina F <polina.fedo@mail.ru>
2023-03-09 17:38:08 +0700

    feat: add photo + cards of models

--- src/assets/components/model-card/model-card.js
+++ src/assets/components/model-card/model-card.js
@@ -1,15 +1,22 @@
-const mobileWidth = window.matchMedia('(max-width: 991.98px)');
+// const mobileWidth = window.matchMedia('(max-width: 991.98px)');
+//
+// function deleteHover(media) {
+//     if (media.matches) {
+//         const hoveredCards = document.querySelectorAll('.model-card.hovered');
+//         hoveredCards.forEach((card) => {
+//             card.classList.remove('hovered');
+//             card.addEventListener('click', () => {
+//                 card.classList.toggle('hovered');
+//             })
+//         })
+//     }
+// }
+//
+// deleteHover(mobileWidth);
 
-function deleteHover(media) {
-    if (media.matches) {
-        const hoveredCards = document.querySelectorAll('.model-card.hovered');
-        hoveredCards.forEach((card) => {
-            card.classList.remove('hovered');
-            card.addEventListener('click', () => {
-                card.classList.toggle('hovered');
-            })
-        })
-    }
-}
-
-deleteHover(mobileWidth);
+const hoveredCards = document.querySelectorAll('.model-card');
+hoveredCards.forEach((card) => {
+    card.addEventListener('click', () => {
+        card.classList.toggle('flip');
+    })
+})
